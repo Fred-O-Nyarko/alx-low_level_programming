f45823cead29f31cc635c2a4b3aac2922592f65e
Frederick Obeng-Nyarko <nernuer@gmail.com>
2022-06-02 18:42:44 -0700

    fix betty formatting issues

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,8 +12,8 @@
 int main(void)
 {
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	long int stringLength = strlen(str);
-	
-	write(2, str, stringLength);
+	long int length = strlen(str);
+
+	write(2, str, length);
 	return (1);
 }
