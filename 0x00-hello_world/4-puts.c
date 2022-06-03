2338621a53ba0b197b077fa4e8276ca22ee603fe
Frederick Obeng-Nyarko <nernuer@gmail.com>
2022-06-02 18:06:03 -0700

    add a c program to print string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program prints a string
+ *
+ * Return:  0 afterwards
+ */
+
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
