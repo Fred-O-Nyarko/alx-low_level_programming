ef291d77fb41172bfec896c222748e1cc75c68b6
Frederick Obeng-Nyarko <nernuer@gmail.com>
2022-06-03 18:24:53 -0700

    advanced scripts

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+# include <stdio.h>
+
+/**
+ * main - Prints the  smallest combination of two digits
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n = 0, s;
+
+	while (n <= 9)
+	{
+		for (s = 0; s <= 9; s++)
+		{
+			if (n < s)
+			{
+				putchar(n + '0');
+				putchar(s + '0');
+				if (n != 8 || (n == 8 && s != 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+  * main - Prints all possible combinations of two two-digit numbers.
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if (((a + b) < (c + d)) && (a <= c))
+					{
+						putchar(a + '0');
+						putchar(b + '0');
+						putchar(' ');
+						putchar(c + '0');
+						putchar(d + '0');
+
+						if (a + b + c + d  != 35)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
