1b09a3136c8f31353e8400588069a83c1cb748a9
Frederick Obeng-Nyarko <nernuer@gmail.com>
2022-06-07 14:31:27 -0700

    add code to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - Prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[7] = { '_', 'p', 'u', 't', 'c', 'h', 'a', 'r' };
+	int i = 0;
+	int length = strlen(str);
+
+	for (i = 0; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	return (0);
+
+}
+
