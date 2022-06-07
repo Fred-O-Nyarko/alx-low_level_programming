da32b8d2a2dd19fc276a2ced22b0919580988ef9
Frederick Obeng-Nyarko <nernuer@gmail.com>
2022-06-07 14:52:10 -0700

    fix bug

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include "main.h"
-#include <string.h>
+# include "main.h"
 
 /**
  * main - Prints _putchar, followed by a new line.
@@ -9,16 +8,15 @@
 
 int main(void)
 {
-	char str[8] = "_putchar";
-	int i = 0;
-	int length = strlen(str);
-
-	for (i = 0; i < length; i++)
-	{
-		_putchar(str[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
-
 }
-
