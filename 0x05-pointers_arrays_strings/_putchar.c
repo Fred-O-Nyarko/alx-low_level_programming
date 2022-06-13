ec6dd6f766351cbe033ec0214696b91f2d5e3eb8
Frederick Obeng-Nyarko <nernuer@gmail.com>
2022-06-13 15:49:59 -0700

    adjust styling to suit betty standards

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -3,9 +3,9 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return 1 if true, 0 if false
+ * Return: 1 if true, 0 if false
  * On error, -1 return.
- * */
+ */
 
 int _putchar(char c)
 {
